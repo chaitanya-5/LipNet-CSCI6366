4b191e4b7669653034f49c09d28791b08dd8ba48
mayankt28 <mayankt28@gmail.com>
2024-11-22 14:41:18 -0500

    Optimize VideoDatasetManager by leveraging tf.data.Dataset.list_files and zip functionality

--- Preprocess/Dataset.py
+++ Preprocess/Dataset.py
@@ -7,12 +7,12 @@ class VideoDatasetManager:
         self.align_dir = align_dir
 
     def get_video_paths(self):
-        return [os.path.join(self.video_dir, f) for f in os.listdir(self.video_dir) if f.endswith('.mp4')]
+        return tf.data.Dataset.list_files(self.video_dir + '/*.mpg', shuffle = False)
 
     def get_align_paths(self):
-        return [os.path.join(self.align_dir, f) for f in os.listdir(self.align_dir) if f.endswith('.align')]
+        return tf.data.Dataset.list_files(self.align_dir + '/*.align', shuffle = False)
 
     def create_dataset(self):
         video_paths = self.get_video_paths()
         align_paths = self.get_align_paths()
-        return tf.data.Dataset.from_tensor_slices((video_paths, align_paths))
+        return tf.data.Dataset.zip((video_paths, align_paths))
